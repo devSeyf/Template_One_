3da05b2f55da4d40e010087b653756d8f082fcc5
saif jamal <saif@localhost.localdomain>
2025-04-27 11:58:00 +0300

    تحديث الأيقونة مع تأثير hover

---
+++ logic.js
@@ -0,0 +1,9 @@
+// أول شيء نحدد العناصر
+var links = document.getElementById("menu");
+var list = document.getElementById("menuList");
+
+
+links.addEventListener("click", function() {
+    
+    list.classList.toggle("show");
+});
