c2854ca2594e8ab54401d0f587d4e40d8f0e2bc2
devSeyf <ssaaiiffjjaammaall@gmail.com>
2025-05-03 10:29:02 +0300

    Add toggle for dark theme

--- logic.js
+++ logic.js
@@ -2,8 +2,29 @@
 var links = document.getElementById("menu");
 var list = document.getElementById("menuList");
 
-
 links.addEventListener("click", function() {
     
     list.classList.toggle("show");
 });
+
+
+
+let toggle = document.getElementById("toggle-theme");
+
+if(localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+toggle.addEventListener("click", function() {
+  document.body.classList.toggle("dark");
+
+  
+  if(document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+
+
